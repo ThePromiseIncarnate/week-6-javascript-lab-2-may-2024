954c0e7f01e4f16e395fdfc6971b8ca8f5e5ed66
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-06-27 18:58:11 -0500

    day 4

--- day-4/script.js
+++ day-4/script.js
@@ -15,11 +15,13 @@ function partOneA () {
 // Try It!
 // Create a loop with a different high number
 function partOneB () {
+  let highNumber = 69;
   alert("Try it!");
   // 1. Create a for loop.
-
+  for (let i = 0; i < highNumber; i++) {
     // 2. give the user and alert with the current number.
-
+    alert(`The number is ${i}!.`);
+  }
 }
 
 // Example
@@ -47,13 +49,14 @@ function partTwoB () {
   messageParagraph.innerHTML = "Flavors";
 
   // 1. Create an array of 5 flavors
-
+  let flavors = ["Chocolate", "Mint Chocolate Chip", "Oreo", "Strawberry", "Vanilla"]; 
   // 2. Find the length of the array
-
+  let highNumber = flavors.length;
   // 3. Create a for loop.
-
+  for (let i = 0; i < highNumber; i++) {
     // 4. give the user and alert with the current friend.
-
+    alert(`The current flavor is ${flavors[i]}.`);
+  }
 }
 
 // Example
@@ -75,16 +78,17 @@ function partThreeA () {
 // Try it!
 // Try to loop over favorite flavors
 function partThreeB () {
+  messageParagraph.innerHTML = "Try it!";
   messageParagraph.innerHTML = "Flavors";
-
-  alert("Try it!");
   // 1. create an array of 5 flavors
-
+  let flavors = ["Chocolate", "Mint Chocolate Chip", "Oreo", "Strawberry", "Vanilla"];
   // 2. find the length of the array
- 
+  let highNumber = flavors.length;
   // 3. create a for loop
-
+  for (let i = 0; i < highNumber; i++) {
     // 4. write the friend to the innerHTML of the list
+    messageList.innerHTML += `<li>${flavors[i]}</li>`;
+  }
 }
 
 // Example
@@ -123,9 +127,9 @@ function partFourB () {
   let newFlavor, highNumber;
 
   // 1. prompt for a flavor
-
+  newFlavor = prompt("What flavor would you like?"); 
   // 2. add the flavor to the array with push()
-
+  flavors.push(newFlavor); 
   // 3. reset the list in the innerHTML
   messageList.innerHTML = "";
   // 4. set the message to "Flavors"
@@ -135,6 +139,8 @@ function partFourB () {
   highNumber = flavors.length;
 
   // 6. write all the flavors back to the HTML list from the array, updated with our new flavor.
-
+  for (let i = 0; i < highNumber; i++) {
+    messageList.innerHTML += `<li>${friends[i]}</li>`;
+  }
 }
 
